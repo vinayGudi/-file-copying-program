8039335b6dd9132fb8c3da511e4170ad0ca4cf20
vinayGudi <37874414+vinayGudi@users.noreply.github.com>
2018-04-04 19:57:26 +0530

    Update main_code.cpp

--- main_code.cpp
+++ main_code.cpp
@@ -2,7 +2,13 @@
 #include<iostream>
 using namepsace std;
 int main(){
-	//to do fab
-
+	#include<iostream>
+#include<pthread.h>
+#include <unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+using namespace std;
+	 int size,first=0,second=1,third,n;
+	 int a[1000];
 
 }
